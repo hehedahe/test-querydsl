b85f8dc4d0a74b5ee2159b8e18d99c61979f53bc
hehedahe <smyekgo@gmail.com>
2022-11-03 17:27:53 +0900

    querydsl: fetch().size()

--- queryDsl/src/test/java/com/example/demo/QueryDslApplicationTests.java
+++ queryDsl/src/test/java/com/example/demo/QueryDslApplicationTests.java
@@ -1,14 +1,11 @@
 package com.example.demo;
 
-import java.util.Iterator;
-import java.util.List;
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import com.example.demo.entity.Member;
 import com.example.demo.entity.QMember;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.extern.slf4j.Slf4j;
@@ -80,32 +77,73 @@ class QueryDslApplicationTests {
   @Test
   public void search() {
 
-    // [select: 데이터 하나 조회하기]
+    // [select 1: 데이터 하나 조회하기]
 
     //    Member findMember = queryFactory
     //        .selectFrom(qMember)
     //        .where(qMember.username.eq("member1")
     //            .and(qMember.age.eq(10)))
-    //        .fetchOne();
-    //
-    //    assertThat(findMember.getUsername()).isEqualTo("member1");
-    //
+    //        .fetchOne(); // 하나의 데이터만 조회한다. 결과가 없다면, null을 반환한다. 두 건 이상일 경우 NonUniqueResultException 발생한다.
     //
     //    log.info("test >>>>>>>> " + findMember.getUsername());
 
 
-    //  [select: 여러 데이터 조회하기]  
-    List<Member> result = queryFactory
-        .selectFrom(qMember)
-        .where(qMember.username.eq("member1")
-            .or(qMember.age.eq(10)))
-        .fetch();
+    //  [select 2: 여러 데이터 조회하기]
 
-    Iterator<Member> resItr = result.iterator();
+    //    List<Member> result = queryFactory
+    //        .selectFrom(qMember)
+    //        .where(qMember.username.eq("member1")
+    //            .or(qMember.age.eq(10)))
+    //        .fetch(); // limit(1).fetch() == fetchFirst()
+    //
+    //    Iterator<Member> resItr = result.iterator();
+    //
+    //    while (resItr.hasNext()) {
+    //      log.info(">>>>> result >>>>> " + resItr.next());
+    //    }
+    //
+    //    //DB에서 가져온 결과값이 없다면, 빈 배열을 리턴한다.
+    //    log.info(">>>>> result >>>>> " + result.toString());
 
-    while (resItr.hasNext()) {
-      log.info(">>>>> result >>>>> " + resItr.next());
-    }
+    // 검색조건
+    // eq(), ne(), eq().not(), isNotNull(), in(), notIn(), between(), goe(), gt(), loe(), lt(), like(), contains(), startsWith() 등이 있다.
+
+
+
+    // [select 4: 데이터 개수 세기]
+    //    int result = queryFactory
+    //        .selectFrom(qMember)
+    //        .where( 
+    //            qMember.username.eq("member1")
+    //            .or(qMember.age.eq(10))
+    //            )
+    //        .fetch().size();
+    //    // fetchCount()를 사용하지 않는 이유? deprecated
+    //    // => fetchResult()의 경우, QueryResults로 count쿼리를 사용하는 것이 완벽하게 지원되지 않기 때문에 fetch()를 사용하여 java에서 List.size() 메서드를 이용할 것을 추천한다.
+    //    //    fetchCount()의 경우에는 fetch.size()를 사용할 것을 추천하고 있다. 
+    //
+    //    log.info(">>>>> result >>>>> " + result);
+  }
+
+
+  @Test
+  public void searchWithParam() {
+    //     [select 3: where()안에 파라미터로 조건 추가하기] = AND조건
+
+    //    List<Member> result = queryFactory
+    //        .selectFrom(qMember)
+    //        .where( // 파라미터 값이 null, 즉 아무 조건도 넣지 않으면 selectFrom(qMember).fetch() 와 같은 동작을 한다. => select1보다 깔끔하게 동적 쿼리를 작성할 수 있다.
+    //            qMember.username.eq("member1"),
+    //            qMember.age.eq(10)
+    //            )
+    //        .fetch();
+    //
+    //    Iterator<Member> resItr = result.iterator();
+    //
+    //    while (resItr.hasNext()) {
+    //      log.info(">>>>> result >>>>> " + resItr.next());
+    //    }
+
 
   }
 
